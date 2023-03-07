7bed004d6dda908028188d6266bb846cf6304a91
Alex Pedersen <mails@alexpedersen.dev>
2023-03-07 22:33:42 +0100

    chores: remove console logs on production

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.md$/i,
-      loader: "raw-loader",
-    });
-
-    // Important: return the modified config
-    return config;
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production"
   },
 }
 
