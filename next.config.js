f0483b470d122aed6bd764c9435e7cc5995af769
Alex Pedersen <mails@alexpedersen.dev>
2023-06-23 22:31:19 +0200

    enable a static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   compiler: {
     removeConsole: process.env.NODE_ENV === "production"
   },
