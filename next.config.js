ddee4ebbefbe4a91ea0fcda488a85170257d3092
Alex Pedersen <hello@alexpedersen.dev>
2024-09-22 16:12:04 +0200

    chore: update next export config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production"
   },
