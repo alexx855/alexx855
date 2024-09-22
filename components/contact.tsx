64a95602fe7bb9a8807a566a23f9d7faa11e2e5d
Alex Pedersen <hello@alexpedersen.dev>
2024-09-22 19:23:36 +0200

    refactor: Update contact section with link tree
    - Removed badges for Stack Overflow, Discord, and Warpcast
    - Added link tree with links to LinkedIn, GitHub, Twitter, Stack Overflow, and Warpcast
    - Updated contact section title to "Link Tree 🌲"

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,29 +1,29 @@
 import styles from "../styles/contact.module.css";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Contact() {
+  const links = [
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/alexx855" },
+    { name: "GitHub", url: "https://github.com/alexx855" },
+    { name: "Twitter", url: "https://twitter.com/alexx855" },
+    { name: "Stack Overflow", url: "https://stackoverflow.com/users/4717076/alex-pedersen" },
+    { name: "Warpcast", url: "https://warpcast.com/alexx855.eth" },
+  ];
+
   return (
     <section id="contact" className={styles.contact}>
-      <h3>Contact</h3>
-      <div className={styles.contactList}>
-        <Link target="_blank" href="https://www.linkedin.com/in/alexx855">
-          <Image src="/badges/LinkedIn.svg" width={111} height={28} alt="Linkedin Badge" />
-        </Link>
-        <Link target="_blank" href="https://github.com/alexx855">
-          <Image src="/badges/Github.svg" width={96} height={28} alt="Github Badge" />
-        </Link>
-        <Link target="_blank" href="https://discord.com/users/alexx855.eth#9229">
-          <Image src="/badges/Discord.svg" width={105} height={28} alt="Discord Badge" />
-        </Link>
-        <Link target="_blank" href="https://twitter.com/alexx855">
-          <Image src="/badges/Twitter.svg" width={104} height={28} alt="Twitter Badge" />
-        </Link>
-        {/* <Link target="_blank" href="https://stackoverflow.com/users/4717076/alex-pedersen">
-          <Image src="/badges/Stack_Overflow.svg" width={164} height={28} alt="Stackoverflow Badge" />
-        </Link> */}
-      </div>
-      <p className={styles.adios}>Adios <span>👋</span></p>
+      <h3 className={styles.title}>Link Tree 🌲</h3>
+      <nav>
+        <ul className={styles.contactList}>
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link href={link.url} target="_blank" rel="noopener noreferrer">
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </section>
   );
 }
